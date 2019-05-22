63802e4a59991f0ae0b8e931500bbc0e00611b39
Michael <polli104@mail.chapman.edu>
2019-05-22 16:47:52 -0700

    added vagrant setup

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <zephyr.h>
+
+void main(void)
+{
+
+}
